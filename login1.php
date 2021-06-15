6608259fe6d4376ca88864c905c7caf91aeac0e5
scottnguyen0412 <74314165+scottnguyen0412@users.noreply.github.com>
2021-06-15 15:40:04 +0700

    Update login1.php

--- login1.php
+++ login1.php
@@ -22,6 +22,7 @@
   <table style="width:100%">
   <thread style="text-align:left">
                 <tr>
+		<p style="color:blue">Product ID<p>
                 <th style="background-color:#FFFF00">Product ID</th>
                 <th style="background-color:#FF7F50">Product Name</th>
                 <th style="background-color:#FFFF00">Product Price</th>
