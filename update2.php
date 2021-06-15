168751b57c8b5da066a209604c8a318085bda41b
scottnguyen0412 <74314165+scottnguyen0412@users.noreply.github.com>
2021-06-15 16:49:31 +0700

    Update update2.php
    update2

--- update2.php
+++ update2.php
@@ -21,7 +21,7 @@ $qt=$_GET['qt'];
  <body>
 <style>
  body {
- background-image: url('Toy1.jpg');
+ background-image: url('updateatn2.png');
  background-attachment: fixed;
  background-size: 100%100%;
  }
