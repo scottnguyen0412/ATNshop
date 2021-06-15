0b1579164593e0595674df0fb24b7744110421dc
scottnguyen0412 <74314165+scottnguyen0412@users.noreply.github.com>
2021-06-15 12:06:47 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,17 +2,15 @@
 <head>
     <link rel="stylesheet" href ="styles.css">
     <title> Login </title>
-    <marquee bgcolor="gray" 
+    <marquee bgcolor="orange" 
              scrollamount="12" 
-             direction="left" 
-             onmouseover="this.stop();" 
-             onmouseout="this.start();">
+             direction="left" >
         <center>
 		<?php
 		date_default_timezone_set('Asia/Ho_Chi_Minh');
 		$date = getdate();
 			echo "<hr>";
-			echo "Today is: ".$date['weekday']."--".$date['mday']."/".$date['mon']."/".$date['year']."--".$date['hours'].":".$date['minutes'].":".$date['seconds'];
+			echo "Today is: ".$date['weekday']."--".$date['mday']."/".$date['mon']."/".$date['year']."--".$date['hours'].":".$date['minutes'].;
 		?>
 	  </center>
     </marquee>
@@ -24,14 +22,15 @@
     <body>
       <style>
         body {
-          
+         
         } 
       </style>
       <div>
-	      <h2 style="color:black;" align="center">LOGIN </h2>
-		<form style="color:black;" align="center" method="POST">
+	      <h2 style="color:violet;" align="center">Login</h2>
+		<form style="color:violet;" align="center" method="POST">
 		Username:<input type="text" name="userid"> <br><br>
-		Password:&nbsp;<input type="password" name="password"> <br><br>
+		Password:&nbsp;<input type="password" name="password"> 
+		
 	<input type="submit" value="LOGIN">
     </form>
     </div>
@@ -41,10 +40,10 @@
     		$user = $_POST['userid'];
     		$pass= $_POST['password'];
     		}
-        	if($user == "shop1" && $pass == "shop1"){
+        	if($user == "ATN1" && $pass == "admin"){
            	header("location:shop1.php");
         	}
-	    	if($user == "shop2" && $pass == "shop2"){
+	    	if($user == "ATN2" && $pass == "admin"){
            	header("location:shop2.php");
         	}
 		if($user == "admin" && $pass == "admin"){
